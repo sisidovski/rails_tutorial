6e17d394603a503e254290972eab42a868985b75
sisidovski <arerreee@gmail.com>
2015-12-30 04:51:46 +0900

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -9,6 +9,6 @@ port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveRecort::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
 
